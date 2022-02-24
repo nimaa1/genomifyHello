cc101020d284f4777fdaae9f79c447423da0e838
nimaa <nima.abedpour@genomify.ai>
2022-02-24 21:24:43 +0100

    third PWA commit

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-var GHPATH = '/github-page-pwa';
-var APP_PREFIX = 'gppwa_';
-var VERSION = 'version_002';
+var GHPATH = '/genomifyHello';
+var APP_PREFIX = 'gpwa_';
+var VERSION = 'version_005';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
   `${GHPATH}/css/styles.css`,
-  `${GHPATH}/img/icon.png`,
+  `${GHPATH}/img/icon1024.png`,
   `${GHPATH}/js/app.js`
 ]
 
