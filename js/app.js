c404ab244dadb21e503e6f8f55a9c8ceb83113de
nimaa <nima.abedpour@genomify.ai>
2022-02-25 01:43:05 +0100

    seventh PWA commit

--- js/app.js
+++ js/app.js
@@ -2,16 +2,17 @@
 
 const form = document.querySelector('form');
 const name = document.querySelector('#name');
-//const name =  form.elemens['name'];
+const hello = document.querySelector("#hello");    
 
 
-const addName = () =>{
+
+const addName = e =>{
   //var name = document.getElementById('name');
-  if (name.value != '') {
-    var hello = document.getElementById("hello");    
-    hello.innerHTML = "Hello " + name.value + " !";
+  if (name.value) {
+    hello.textContent = "Hello " + name.value + " !";
     name.value = '';
   }
+  e.preventDefault();
 };
 
 form.addEventListener('submit', addName);
